cbbaee117912e66d8924f996ffb1840e13b2b005
yuliabgkv <bugakovayk@gmail.com>
2024-02-18 00:28:52 +0200

    fix filter settings

--- src/components/FilterSettings/FilterSettings.js
+++ src/components/FilterSettings/FilterSettings.js
@@ -4,40 +4,38 @@ import styles from './FilterSettings.module.css';
 function FilterSettings(props) {
     return (
         <>
-            {(props.gender || props.species || props.status) && 
-                <div className={styles['filter-settings']}>
-                    {props.name &&
-                        <div className={styles['name-filter-message']}>
-                            Search results by name "{props.name}" 
-                            <span 
-                                className={'reset-filter-btn'}
-                                onClick={() => props.resetFilters('name')}
-                            ></span>
-                        </div>
-                    }
-                    {props.gender && 
-                        <FilterSelection
-                            filterName='gender'
-                            filterValue={props.gender}
-                            resetFilters={props.resetFilters}
-                        />
-                    }
-                    {props.species && 
-                        <FilterSelection
-                            filterName='species'
-                            filterValue={props.species}
-                            resetFilters={props.resetFilters}
-                        />
-                    }
-                    {props.status && 
-                        <FilterSelection
-                            filterName='status'
-                            filterValue={props.status}
-                            resetFilters={props.resetFilters}
-                        />
-                    }
-                </div>
-            }
+            <div className={styles['filter-settings']}>
+                {props.name &&
+                    <div className={styles['name-filter-message']}>
+                        Search results by name "{props.name}" 
+                        <span 
+                            className={'reset-filter-btn'}
+                            onClick={() => props.resetFilters('name')}
+                        ></span>
+                    </div>
+                }
+                {props.gender && 
+                    <FilterSelection
+                        filterName='gender'
+                        filterValue={props.gender}
+                        resetFilters={props.resetFilters}
+                    />
+                }
+                {props.species && 
+                    <FilterSelection
+                        filterName='species'
+                        filterValue={props.species}
+                        resetFilters={props.resetFilters}
+                    />
+                }
+                {props.status && 
+                    <FilterSelection
+                        filterName='status'
+                        filterValue={props.status}
+                        resetFilters={props.resetFilters}
+                    />
+                }
+            </div>
         </>
     );
 }
