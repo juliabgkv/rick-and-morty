301f8224b01fa836a1d0ec229fda5debfc5e4d77
yuliabgkv <bugakovayk@gmail.com>
2024-02-19 18:13:33 +0200

    change loader spinner

--- src/components/UI/LoadingSpinner.js
+++ src/components/UI/LoadingSpinner.js
@@ -3,7 +3,7 @@ import styles from './LoadingSpinner.module.css';
 function LoadingSpinner() {
   return (
     <div className={styles['loader-container']}>
-        <span className={styles.loader}></span>
+        <div className={styles['lds-spinner']}><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
     </div>
   );
 }
