1eca0d56e288131283c7a1a6d230253088a3be75
yuliabgkv <bugakovayk@gmail.com>
2024-06-15 11:24:41 +0300

    fix(episodes page): fix loader spinner position

--- src/pages/EpisodesPage.js
+++ src/pages/EpisodesPage.js
@@ -11,27 +11,27 @@ function EpisodesPage() {
 
   useEffect(() => {
     document.title = 'Episodes';
-    
+
     async function fetchEpisodes() {
       setLoading(true);
       let response = await fetch(`${API_URL}episode`);
       let data = await response.json();
 
-      if(data.error) {
+      if (data.error) {
         setError(data.error);
         setLoading(false);
       } else {
         let episodesArr = data.results;
         let nextUrl = data.info.next;
-  
-        while(nextUrl) {
+
+        while (nextUrl) {
           response = await fetch(nextUrl);
           data = await response.json();
-  
+
           episodesArr = episodesArr.concat(data.results);
           nextUrl = data.info.next;
         }
-        
+
         setEpisodes(episodesArr);
         setLoading(false);
       }
@@ -40,19 +40,31 @@ function EpisodesPage() {
     fetchEpisodes();
   }, []);
 
-  return (
-    <div className={styles['episodes-container']}>
-        {loading && <LoadingSpinner />}
-        {!loading && episodes && episodes.map(episode => (
+  let content;
+
+  if (loading) {
+    content = (
+      <div className={styles['loader-container']}>
+        <LoadingSpinner />
+      </div>
+    );
+  } else if (!loading && episodes) {
+    content = (
+      <div className={styles['episodes-container']}>
+        {episodes.map((episode) => (
           <Card key={episode.id} className={styles['episode-card']}>
             <div className={styles['episode-name']}>{episode.name}</div>
             <div className={styles['episode-date']}>{episode.air_date}</div>
             <div className={styles['episode']}>{episode.episode}</div>
           </Card>
         ))}
-        {error && <p className='error-message'>{error}</p>}
-    </div>
-  );
+      </div>
+    );
+  } else if (error) {
+    content = <p className='error-message'>{error}</p>;
+  }
+
+  return content;
 }
 
 export default EpisodesPage;
